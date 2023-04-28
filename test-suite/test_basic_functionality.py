d20191a0886b2f7e0e3d24dfa3e0d4029f7acf11
Radu Andries <me@admiral0.it>
2023-04-28 13:51:07 +0200

    Formatting for first test

--- test-suite/test_basic_functionality.py
+++ test-suite/test_basic_functionality.py
@@ -2,6 +2,7 @@ import pytest
 import os
 from subprocess import check_output, CalledProcessError
 
+
 def test_app_exists():
 	if not os.path.exists('./build/ddm'):
 		assert False, "Have you built ddm?"
@@ -11,4 +12,4 @@ def test_app_exists():
 def test_app_displays_help_and_exits():
 	with pytest.raises(CalledProcessError):  # ddm returns 100 on help message
 		output = check_output(['./build/ddm', '-h'])
-		assert 'diff' in output, 'diff command in help message'
+		assert 'diff'.encode() in output, 'diff command in help message'
